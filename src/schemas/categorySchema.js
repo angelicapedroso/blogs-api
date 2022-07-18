68df9b48678fb68d07630e8bbfbaed36b9f59807
angelicapedroso <angelicapedroso05@gmail.com>
2022-07-18 15:42:12 -0300

    refactor: remove joi validation in category

--- src/schemas/categorySchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const msg = '"name" is required';
-
-const categorySchema = Joi.object({
-  name: Joi.string().required().messages({ 'string.required': msg }),
-});
-
-module.exports = { categorySchema };
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,12 +1,9 @@
 const { Category } = require('../database/models');
-const { categorySchema } = require('../schemas/categorySchema');
 
 const create = async (name) => {
-  const { error } = categorySchema.validate(name);
-  if (error) {
-    return { status: 400, result: { message: error.message } };
+  if (!name || !name.length) {
+    return { status: 400, result: { message: '"name" is required' } };
   }
-
   const category = await Category.create({ name });
   return { status: 201, result: category };
 };
